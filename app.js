689b21b8226a572a9d39a0e9b8e7401b5b24bbb3
Beknazar Suranchiyev <Suranchiyev96@gmail.com>
2019-02-18 15:14:20 -0500

    core7

--- app.js
+++ app.js
@@ -92,7 +92,7 @@ app.use(function(err, req, res, next) {
 
 //CORS
 app.use(function(req, res, next) {
-    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Origin", "localhost:4200");
     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
     next();
 });
