3369d88888c9a52cca01f93eaba51022e06ada37
Beknazar Suranchiyev <Suranchiyev96@gmail.com>
2019-02-18 11:29:46 -0500

    CORS01

--- app.js
+++ app.js
@@ -47,7 +47,6 @@ app.use(function(req, res, next) {
    res.header("Access-Control-Allow-Origin", "*");
    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
    next();
-
     var err = new Error('Not Found');
     err.status = 404;
     next(err);
@@ -84,5 +83,12 @@ app.use(function(err, req, res, next) {
         error: {}
     });
 });
+
+//CORS
+app.use(function(req, res, next) {
+   res.header("Access-Control-Allow-Origin", "*");
+   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+   next();
+}
 
 module.exports = app;
