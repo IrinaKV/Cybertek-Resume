86024d9277725e2ddaea5a1d38b7ab9e48094628
Beknazar Suranchiyev <Suranchiyev96@gmail.com>
2019-02-20 17:36:31 -0500

    cors 22

--- app.js
+++ app.js
@@ -33,17 +33,19 @@ app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
-//app.use(cors());
 
-const originsWhitelist  = ['http://localhost:4200','https://cybertek-frontend.herokuapp.com']
-var corsOptions = {
-    origin: function(origin, callback){
-        var isWhitelisted = originsWhitelist.indexOf(origin) !== -1;
-        callback(null, isWhitelisted);
-    },
-    credentials:true
-}
-app.use(cors(corsOptions));
+app.use(cors());
+app.options('*', cors());
+
+// const originsWhitelist  = ['http://localhost:4200','https://cybertek-frontend.herokuapp.com']
+// var corsOptions = {
+//     origin: function(origin, callback){
+//         var isWhitelisted = originsWhitelist.indexOf(origin) !== -1;
+//         callback(null, isWhitelisted);
+//     },
+//     credentials:true
+// }
+// app.use(cors(corsOptions));
 
 // [SH] Initialise Passport before using the route middleware
 
