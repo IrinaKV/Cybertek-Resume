e50c6dc6924a0eeb0bde0835f24cdcbea716a4d3
Beknazar Suranchiyev <Suranchiyev96@gmail.com>
2019-02-20 19:30:25 -0500

    s3 cred

--- api/util/s3server.js
+++ api/util/s3server.js
@@ -2,25 +2,14 @@ const multer = require('multer');
 const multerS3 = require('multer-s3');
 const aws = require('aws-sdk');
 
-//const secretAccessKey = process.env.SECRETKEY.toString().trim() ;
-//const accessKeyId = process.env.KEYID.toString().trim();
+const secretAccessKey = process.env.SECRETKEY.toString().trim() ;
+const accessKeyId = process.env.KEYID.toString().trim();
 
 aws.config.update({
-    // Your SECRET ACCESS KEY from AWS should go here,
-    // Never share it!
-    // Setup Env Variable, e.g: process.env.SECRET_ACCESS_KEY
-      secretAccessKey: "zOsO6QeGaVVCT18nUhD1VaKerg1eP1NdOjZ+sY7K",
-      //secretAccessKey: secretAccessKey,
-    // Not working key, Your ACCESS KEY ID from AWS should go here,
-    // Never share it!
-    // Setup Env Variable, e.g: process.env.ACCESS_KEY_ID
-
-//      accessKeyId: "AKIAIXQWQ2VMX4DGNDCA",
-      accessKeyId: "AKIAIHXX5UXQBLHEPDIA",
-    //accessKeyId: accessKeyId,
-    region: 'us-east-1' // region of your bucket
+    secretAccessKey: secretAccessKey,
+    accessKeyId: accessKeyId,
+    region: 'us-east-1'
 });
-
 
 const s3 = new aws.S3();
 
@@ -30,8 +19,6 @@ const upload = multer({
         bucket: 'cybertek-resume',
         acl: 'public-read',
         metadata: function (req, file, cb) {
-            // console.log("AccessKey: "+secretAccessKey);
-            // console.log("KeyID: "+accessKeyId);
             cb(null, {fieldName: file.fieldname});
         },
         key: function (req, file, cb) {
